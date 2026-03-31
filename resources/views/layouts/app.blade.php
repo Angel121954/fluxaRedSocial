9fc114cd43e82a610771f9bb129835f6e681eba0
Fluxa <angeldavidagudelocuartas13@gmail.com>
2026-03-31 15:15:13 -0500

    chore(seo): Mejoras de SEO para posicionamiento en la web de la red social en el corazon de las vistas (layouts/app)

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,22 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" dir="ltr">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
+    <meta name="description" content="Fluxa - Plataforma para desarrolladores que quieren compartir su progreso, aprender juntos y crecer como comunidad.">
+    <meta name="keywords" content="Fluxa, desarrollo, programación, comunidad, proyectos, open source">
+
+    <!-- Open Graph -->
+    <meta property="og:title" content="@yield('title', 'Fluxa')">
+    <meta property="og:description" content="Plataforma para desarrolladores que quieren compartir su progreso, aprender juntos y crecer como comunidad.">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{ request()->url() }}">
+    <meta property="og:image" content="{{ asset('img/logoFluxa.png') }}">
+
     <title>@yield('title', 'Fluxa')</title>
+    <link rel="icon" type="image/png" href="{{ asset('img/logoFluxa.png') }}">
     <link rel="preconnect" href="https://fonts.bunny.net">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
     <link rel="stylesheet" href="{{ asset('css/variables.css') }}">
