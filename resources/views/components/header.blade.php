f173707283a3e510df66784c8886e6e9d5a2b205
Fluxa <angeldavidagudelocuartas13@gmail.com>
2026-06-10 13:56:12 -0500

    perf(login): Implementación de mejor diseño y jerarquia visual en el login permitiendo una mejor UI

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,14 +1,3 @@
-<!-- Header -->
-<div class="mb-5">
+<div class="mb-4">
     <img src="{{ asset('img/logoFluxa.png') }}" alt="Fluxa" class="logo-img" />
-    <p
-        style="
-                color: var(--text-secondary);
-                font-size: 0.9375rem;
-                line-height: 1.5;
-                max-width: 420px;
-              ">
-        La red social donde los desarrolladores comparten proyectos,
-        crecen en público y conectan con otros builders.
-    </p>
 </div>
