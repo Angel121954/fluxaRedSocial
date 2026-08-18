2fafd08c66e9702c9752db23ee32144a1a5da8b5
Fluxa <angeldavidagudelocuartas13@gmail.com>
2026-08-18 18:44:24 -0500

    fix(auth): Agregar import de Request faltante en NewPasswordController
    El método create() tipaba Request  sin importar
    Illuminate\Http\Request, causando que PHP resolviera
    App\Http\Controllers\Auth\Request que no existe.

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -6,6 +6,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ResetPasswordRequest;
+use Illuminate\Http\Request;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Support\Facades\Hash;
